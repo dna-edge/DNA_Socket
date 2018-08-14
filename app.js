267850564f9739721aea277f185d27796ee67780
SoyoungPark <3457soso@naver.com>
2018-08-15 03:34:22 +0900

    구조 재설정

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-global.env = require('./env');
+require('dotenv').config();
 global.utils = require('./utils/global');
 require('./routes')(app);
 
