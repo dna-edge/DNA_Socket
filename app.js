625e04fbe693b225c9579af17ac52b7ffbe2b9ef
SoyoungPark <3457soso@naver.com>
2018-08-19 00:06:47 +0900

    SSL

--- app.js
+++ app.js
@@ -16,28 +16,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 require('dotenv').config();
 global.utils = require('./utils/global');
 require('./routes')(app);
-app.get('/message', function(req, res){
-  res.sendFile(__dirname + '/test_message.html');
-});
 
-app.get('/dm', function(req, res){
-  res.sendFile(__dirname + '/test_dm.html');
-});
 
-const PORT = 9013;
-const server = require('http').Server(app);
-const socket = require('./utils/socket').init(server);
-
+/* Error Handler*/
 process.stdin.resume(); //so the program will not close instantly
 
 function exitHandler(options, exitCode) {
-    if (options.cleanup || exitCode || exitCode === 0) {
-      global.utils.redis.del("clients");
-    }
-    if (options.exit) {
-      global.utils.redis.del("clients");
-      process.exit();
-    }
+  if (options.cleanup || exitCode || exitCode === 0) {
+    global.utils.redis.del("clients");
+  }
+  if (options.exit) {
+    global.utils.redis.del("clients");
+    process.exit();
+  }
 }
 
 process.on('exit', exitHandler.bind(null,{cleanup:true}));            // do something when app is closing
@@ -46,9 +37,41 @@ process.on('SIGUSR1', exitHandler.bind(null, {exit:true}));           // catches
 process.on('SIGUSR2', exitHandler.bind(null, {exit:true}));
 // process.on('uncaughtException', exitHandler.bind(null, {exit:true})); // uncaught exceptions
 
-server.listen(PORT, 'localhost', () => {
-  console.info('[DNA-SocketApiServer] Listening on port %s at %s', 
-  server.address().port, server.address().address);
+/* SSL */
+const lex = require('greenlock-express').create({
+  version: 'v02',
+  configDir: '/etc/letsencrypt',
+  server: 'staging',
+  approveDomains: (opts, certs, cb) => {
+    if (certs) {
+      opts.domains = ['localhost', '127.0.0.1'];
+    } else {
+      opts.email = 'soyoungpark.me@gmail.com';
+      opts.agreeTos = true;
+    }
+    cb(null, { options: opts, certs });
+  },
+  renewWithin: 81 * 24 * 60 * 60 * 1000,
+  renewBy: 80 * 24 * 60 * 60 * 1000,
 });
 
+const server = require('http')
+  .createServer(lex.middleware(require('redirect-https')()))
+  
+const socket = require('./utils/socket').init(server);
+server.listen(process.env.PORT);
+
 module.exports = app;
+
+// server.listen(PORT, 'localhost', () => {
+//   console.info('[DNA-SocketApiServer] Listening on port %s at %s', 
+//   server.address().port, server.address().address);
+// });
+
+// app.get('/message', function(req, res){
+//   res.sendFile(__dirname + '/test_message.html');
+// });
+
+// app.get('/dm', function(req, res){
+//   res.sendFile(__dirname + '/test_dm.html');
+// });
