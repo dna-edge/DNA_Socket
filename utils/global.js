46fd88be3ceeac21a577d683e06c2a67e1274de9
SoyoungPark <3457soso@naver.com>
2018-08-29 17:16:53 +0900

    mysql 커넥션 문제 해결

--- utils/global.js
+++ utils/global.js
@@ -6,13 +6,36 @@ redis.auth(process.env.REDIS_PASSWORD);
 
 /* mysql */
 const mysql = require('mysql');
-const connection = mysql.createConnection({
+const dbConfig = {
   host: process.env.MYSQL_HOST,
   port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.DB_NAME
-});
+};
+
+let connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(dbConfig); 
+
+  connection.connect(function(err) {             
+    if(err) {                                    
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); 
+    }                                     
+  });                                     
+                                          
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+      handleDisconnect();                         
+    } else {                                      
+      throw err;                                  
+    }
+  });
+}
+handleDisconnect();
 
 /* mongodb */
 const mongoose = require('mongoose');
