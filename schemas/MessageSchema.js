a3d31fbf0057540e0953877b7075ad5b7c3360df
SoyoungPark <3457soso@naver.com>
2018-09-05 12:10:08 +0900

    베스트챗 조회 방식 변경

--- schemas/MessageSchema.js
+++ schemas/MessageSchema.js
@@ -83,7 +83,7 @@ Schema.createSchema = (mongoose) => {
   // selectBest : 베스트챗 조회하기
   messageSchema.static('selectBest', function(conditions, callback) {
     /* where 안에 들어가는 이름은 해당 컬럼의 이름임에 주의한다! */
-    return this.find({}, callback)
+    return this.find({ 'like_count': { $gt: 0}}, callback)
       .where('position')
       .within(
         {
